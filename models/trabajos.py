2f280adbe8ff0b44ff6135b2a3a2e96ddfbe03e0
benjamingonzalezc <benjamingonzalezc@alu.unach.cl>
2025-11-21 16:13:26 -0300

    Benja: Restauración de Factories e integración con models

--- models/trabajos.py
+++ models/trabajos.py
@@ -6,6 +6,9 @@ class Trabajo(db.Model):
     __tablename__ = 'trabajos'
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String(200), nullable=False)
+    duracion_meses = db.Column(db.Integer)
+    nota_aprobacion = db.Column(db.Float)
+    requisito_aprobacion = db.Column(db.String(255))
     
     # Relaciones (Foreign Keys)
     tipo_id = db.Column(db.Integer, ForeignKey('tipos_trabajo.id'))
