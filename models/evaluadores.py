b8ab55e8f4bf0dfa4f5f7d7f876691292fb1fd82
adanramos-cmd <adanramos@alu.unach.cl>
2025-11-22 17:04:10 -0300

    Update evaluadores.py
    Corrección detalles codigo

--- models/evaluadores.py
+++ models/evaluadores.py
@@ -1,8 +1,8 @@
 from database import db
 
-class Evaluadores(db.model):
+class Evaluadores(db.Model):
     __tablename__ = 'evaluadores'
-    id = db.Column(db.Ineteger), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(150), notnull=True)
     email = db.Column(db.String(120))
     rol = db.Column(db.String(100))
