fd173367e0010cb17e566011b63c05b0e976c926
adanramos-cmd <adanramos@alu.unach.cl>
2025-11-22 22:50:52 -0300

    mejorar modelo evaluadores
    corregí detalles chicos en el código en si

--- models/evaluadores.py
+++ models/evaluadores.py
@@ -3,7 +3,7 @@ from database import db
 class Evaluadores(db.Model):
     __tablename__ = 'evaluadores'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(150), notnull=True)
+    nombre = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(120))
     rol = db.Column(db.String(100))
     tipo = db.Column(db.String(50))
