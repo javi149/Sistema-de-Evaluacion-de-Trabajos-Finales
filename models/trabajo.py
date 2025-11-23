1df7958b3d9d14dde4ea7eab74537f461d91d874
adanramos-cmd <adanramos@alu.unach.cl>
2025-11-22 22:51:32 -0300

    mejorar modelo trabajo
    agregué entidades faltantes y generé relación con estudiante

--- models/trabajo.py
+++ models/trabajo.py
@@ -5,4 +5,9 @@ class Trabajo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String(200))
     tipo = db.Column(db.String(100)) # Tesis, Proyecto, etc.
+    estado = db.Column(db.String(50), default='PENDIENTE')
+    fecha_entrega = db.Column(db.Date)
     estudiante_id = db.Column(db.Integer, db.ForeignKey('estudiantes.id'))
+    
+    # Relación con Estudiante
+    estudiante = db.relationship('Estudiante', backref='trabajos')
